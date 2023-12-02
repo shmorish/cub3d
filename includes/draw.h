8c0608b59952a4d376f1d16e3e8925b5eac1c3ed
pino <136807429+pinohikari@users.noreply.github.com>
2023-12-02 15:20:14 +0900

    norm wo naoshi tai

--- includes/draw.h
+++ includes/draw.h
@@ -6,7 +6,7 @@
 /*   By: hhino <hhino@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 20:03:47 by hhino             #+#    #+#             */
-/*   Updated: 2023/12/01 18:19:42 by hhino            ###   ########.fr       */
+/*   Updated: 2023/12/02 15:14:55 by hhino            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,18 @@
 
 typedef struct s_data		t_data;
 typedef struct s_wall		t_wall;
+typedef struct s_texture	t_texture;
 
-void	draw_floor_sky(t_data *data);
-void	draw_wall(t_data *data);
-void	draw_minimap(t_data *data);
-void	wall_image_to_window(t_data *data, int w, int h);
-void	re_draw(t_data *data);
+void			draw_floor_sky(t_data *data);
+void			draw_wall(t_data *data);
+void			draw_minimap(t_data *data);
+void			wall_image_to_window(t_data *data, int w, int h);
+void			re_draw(t_data *data);
 
-void	put_file_to_image(t_data *data);
-void	get_wall_addr(t_data *data);
-unsigned int	calculate_wall_coordinates(t_data *data, t_wall *wall, double wallpos, int x, int y);
+//xpm_to_wall
+void			put_file_to_image(t_data *data);
+void			get_wall_addr(t_texture *texture);
+// unsigned int	calculate_wall_coordinates(t_data *data, t_wall *wall, double wallpos, int x, int y);
+unsigned int	calculate_wall_coordinates(t_wall *wall, double wallpos, int visual_height, int y);
 
 #endif
